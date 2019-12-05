80639a893c8540a619e71569a27dde6c5fd5f00d
Christian Mains <work88@live.com>
2019-12-05 13:19:41 -0600

    check in

--- src/uniDB/GymRental.java
+++ src/uniDB/GymRental.java
@@ -3,6 +3,10 @@ package uniDB;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @ author Christian Mains
+ * creates an instance of an item or items rented out to a university user.
+ */
 public class GymRental {
     public user renter;
     private List<GymEquipment> rent;
@@ -16,6 +20,11 @@ public class GymRental {
         rent.add(item);
     }
 
+    /**
+     * Helper Method, called by rent item, returns user of username, if they exist
+     * @param username
+     * @return
+     */
     public user findUser(String username){
         user answer;
         student stu = database.findStudent(username);
@@ -38,6 +47,12 @@ public class GymRental {
         return this.rent;
     }
 
+    /**
+     * Rents out item with rent id to user with renter username if both exists.
+     * @param renter username of person renting out item.
+     * @param rent id if item to be rented
+     * @return String identifying success or type of failure
+     */
     public String rentitem(String renter, String rent){
         user name = findUser(renter); if (name == null){return "invalid user name";}
         GymEquipment equip = database.findGymEquipment(rent); if (equip == null) { return "invalid equipment id";}
@@ -49,6 +64,12 @@ public class GymRental {
         current.additem(equip);
         return "item success cheched out to "+ name.getUsername();
     }
+
+    /**
+     * checks in item from user who it is checked out to
+     * @param id of item to be cheched in.
+     * @return String identifying successs or type of failure
+     */
     public String checkinitem(String id){
         GymEquipment eq = database.findGymEquipment(id);
         GymRental check = database.findGymRent(id); if (check == null) { return " invalid id or item not checked out";}
@@ -58,6 +79,12 @@ public class GymRental {
         }
         return "succefful";
     }
+
+    /**
+     * Displays all item schiched out to this particular user.
+     * @param username of person who has items checked out.
+     * @return String of items checked out or statement of user has no items checked out.
+     */
     public String userrentlist(String username){
         GymRental gr = database.findGymRent(username); if (gr == null) {return "user rental is clear";}
         List<GymEquipment> ge = gr.getRent();  String answer = "";
